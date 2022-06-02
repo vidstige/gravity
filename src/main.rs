85e8a0922f8c9593227a8ca1fb09496a199ff63e
Samuel Carlsson <samuel.carlsson@volumental.com>
2022-06-02 10:52:58 +0200

    random + gravity equation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::Write;
+use std::{thread, time};
+use rand::Rng;
 
 type Resolution = (usize, usize);
 
@@ -32,11 +34,18 @@ impl Vec2<f32> {
     fn sub(self, rhs: Vec2<f32>) -> Self {
         Vec2{x: self.x - rhs.x, y: self.y - rhs.y}
     }
+    fn scale(self, rhs: f32) -> Self {
+        Vec2{x: self.x * rhs, y: self.y * rhs}
+    }
+    fn norm2(self) -> f32 {
+        return self.x * self.x + self.y * self.y;
+    }
 }
 
 struct Star {
     p: Vec2<f32>,
     v: Vec2<f32>,
+    m: f32,
 }
 
 struct Zoom {
@@ -61,8 +70,8 @@ struct Color {
 const WHITE: Color = Color {r: 255, g: 255, b: 255, a: 128};
 
 fn draw_pixel(frame: &mut Frame, x: usize, y: usize, color: Color) {
-    let bpp = 4;
     let (width, _) = frame.resolution;
+    let bpp = 4;
     let stride = width * bpp;
     let index = y * stride + x * bpp;
     frame.pixels[index + 0] = color.r;
@@ -71,20 +80,68 @@ fn draw_pixel(frame: &mut Frame, x: usize, y: usize, color: Color) {
     frame.pixels[index + 3] = color.a;
 }
 
+fn inside(p: Vec2<f32>, resolution: Resolution) -> bool {
+    let (width, height) = resolution;
+    p.x >= 0.0 && p.x < width as f32 && p.y >= 0.0 && p.y < height as f32
+}
+
 fn draw(frame: &mut Frame, stars: &Vec<Star>, zoom: &Zoom) {
     for star in stars {
         let screen = zoom.to_screen(star.p);
-        draw_pixel(frame, screen.x as usize, screen.y as usize, WHITE);
+        if inside(screen, frame.resolution) {
+            draw_pixel(frame, screen.x as usize, screen.y as usize, WHITE);
+        }
     }
 }
 
+fn gravity_forces(stars: &Vec<Star>) -> Vec<Vec2<f32>> {
+    let mut forces: Vec<Vec2<f32>> = stars.iter().map(|_| Vec2::zero()).collect();
+    for i in 0..stars.len()-1 {
+        for j in i+1..stars.len()  {
+            let si = &stars[i];
+            let sj = &stars[j];
+            let delta = si.p.sub(sj.p);
+            let r2 = delta.norm2();
+            let f = G * si.m * sj.m / r2;  // gravity force
+            let r = r2.sqrt();
+            forces[i] = forces[i].add(delta.scale(-f / r));
+            forces[j] = forces[j].add(delta.scale(f / r));
+        }
+    }
+    forces
+}
+
+const G: f32 = 0.001;
+fn step(stars: &mut Vec<Star>, dt: f32) {
+    let forces = gravity_forces(stars);
+    for i in 0..stars.len() {
+        let force = forces[i];
+        let star = &mut stars[i];
+        let acceleration = force.scale(1.0 / star.m);
+        star.p = star.p.add(star.v.scale(dt));
+        star.v = star.v.add(acceleration.scale(dt));
+    }
+}
+
+const FPS: f32 = 30.0;
 fn main() -> std::result::Result<(), std::io::Error> {
     let mut frame = Frame::new((506, 253));
-    let zoom = Zoom{center: Vec2::zero(), scale: 1.0, resolution: frame.resolution};
+    let zoom = Zoom{center: Vec2::zero(), scale: 100.0, resolution: frame.resolution};
     let mut stars: Vec<Star> = vec!();
-    stars.push(Star{p: Vec2::zero(), v: Vec2::zero()});
+    let mut rng = rand::thread_rng();
+    for _ in 0..10 {
+        let p = Vec2::make(
+            rng.gen::<f32>() - 0.5,
+            rng.gen::<f32>() - 0.5,
+        );
+        stars.push(Star{p: p, v: Vec2::zero(), m: 1.0});
+    }
+
+    let dt = 1.0 / FPS as f32;
     loop {
+        step(&mut stars, dt);
         draw(&mut frame, &stars, &zoom);
         std::io::stdout().write_all(&(frame.pixels)).unwrap();
+        thread::sleep(time::Duration::from_secs_f32(dt));
     }
 }

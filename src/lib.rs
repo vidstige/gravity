7186c27dfe3202d8c4c95cb66c45ebbeb0c09d00
Samuel Carlsson <samuel.carlsson@volumental.com>
2022-06-12 21:25:20 +0200

    canvas rendering from rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod gravity;
 
 use crate::gravity::Vec2;
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-
 
 #[wasm_bindgen]
 pub struct Simulation(gravity::Simulation);
@@ -14,27 +14,54 @@ impl Simulation {
         Simulation{0: gravity::Simulation::new()}
     }
     #[wasm_bindgen]
-    pub fn wow(&mut self) -> f32 {
-        self.0.add(&Vec2{x: 0.0, y: -5.0}, &Vec2{x: 0.1, y: 0.0}, 0.1);
-        self.0.add(&Vec2{x: 0.0, y: 5.0}, &Vec2{x: -0.1, y: 5.0}, 0.1);
-        5.5
+    pub fn add(&mut self, x: f32, y: f32, vx: f32, vy: f32, mass: f32) {
+        self.0.add(&Vec2{x: x, y: y}, &Vec2{x: vx, y: vy}, mass);
     }
 }
-
 
 #[wasm_bindgen]
 pub fn step(simulation: &mut Simulation, dt: f32) {
     gravity::step(&mut simulation.0, dt);
 }
 
-/*
+#[wasm_bindgen]
+pub fn render(target: String) {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id(&target).unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, std::f64::consts::TAU)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, 0.5 * std::f64::consts::TAU).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, std::f64::consts::TAU)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, std::f64::consts::TAU)
+        .unwrap();
+
+    context.stroke();
 }
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-*/
